0efe73a91b11a697cc503b7a36436b2d1a4ea235
Toastiin <71255603+Toastiin@users.noreply.github.com>
2020-09-14 10:48:26 +0200

    Initial commit
    Initial commit with game folders/code

---
+++ Assets/Scripts/scr_Clicking.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class scr_Clicking : MonoBehaviour
+{
+    public Text counter;
+    public float counterNumber;
+    public float ammountPerClick == 1.0f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        counter.text = counterNumber + " Cookies";
+    }
+
+    public void Clicked()
+    {
+        counterNumber += ammountPerClick
+    }
+}
